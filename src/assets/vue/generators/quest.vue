02844c2da25cdfa413b5e1bc394692467aa8b4b6
Nassar Zeitoune <nassar.z@slair.com.au>
2017-11-20 00:13:52 +1100

    Added traits to Quest NPCS. Updated Quest views

--- src/assets/vue/generators/quest.vue
+++ src/assets/vue/generators/quest.vue
@@ -7,27 +7,42 @@
 				<p class="bottom-border"><strong>Starter</strong><p>
 				<p><em>Starters</em> are potential methods for your players to discover the quest (or lists the main people interested in it).</p>
 				<ol>
-					<li v-for="(n, i) in values.starter" :key="i">{{n | capitalize}}</li>
+					<li v-for="(n, i) in values.starter" :key="i">
+						{{n.name | capitalize}} 
+						<ul v-for="(o, j) in n.traits" :key="j" v-if="n.traits.length > 0">
+							<li>{{o | capitalize}}</li>
+						</ul>
+					</li>
 				</ol>
 				<p class="bottom-border"><strong>Type</strong></p>
 				<p><em>Type</em> determines what the quest will involve.</p>
+				<ul>
+					<li>
+						{{values.quest.name | capitalize}} 
+						<ul v-for="(o, j) in values.quest.traits" :key="j" v-if="values.quest.traits.length > 0">
+							<li>{{o | capitalize}}</li>
+						</ul>
+					</li>
+				</ul>
+				<p class="bottom-border"><strong>Neutral</strong></p>
+				<p><em>Neutrals</em> are significantly involved, but do not care how the quest is resolved.</p>
 				<ol>
-					<li v-for="(n, i) in values.type" :key="i">{{n | capitalize}}</li>
-				</ol>
-				<p class="bottom-border"><strong>Modifier</strong></p>
-				<p><em>Modifiers</em> flavours the quest and determines any additional complications.</p>
-				<ol>
-					<li v-for="(n, i) in values.modifier" :key="i">{{n | capitalize}}</li>
-				</ol>
-				<p class="bottom-border"><strong>Allies</strong></p>
-				<p><em>Allies</em> want to see a quest succeed.</p>
-				<ol>
-					<li v-for="(n, i) in values.allies" :key="i">{{n | capitalize}}</li>
+					<li v-for="(n, i) in values.neutral" :key="i">
+						{{n.name | capitalize}} 
+						<ul v-for="(o, j) in n.traits" :key="j" v-if="n.traits.length > 0">
+							<li>{{o | capitalize}}</li>
+						</ul>
+					</li>
 				</ol>
 				<p class="bottom-border"><strong>Antagonists</strong></p>
 				<p><em>Antagonists</em> desire to see the quest fail.</p>
 				<ol>
-					<li v-for="(n, i) in values.antagonists" :key="i">{{n | capitalize}}</li>
+					<li v-for="(n, i) in values.antagonists" :key="i">
+						{{n.name | capitalize}} 
+						<ul v-for="(o, j) in n.traits" :key="j" v-if="n.traits.length > 0">
+							<li>{{o | capitalize}}</li>
+						</ul>
+					</li>
 				</ol>
 				<p class="bottom-border"><strong>Locations</strong></p>
 				<p><em>Locations</em> are the main places where the quest should take place.</p>
@@ -49,32 +64,54 @@
 				quests: this.$bsFactory.getTemplate('quests'),
 				values: {
 					starter: [],
-					type: [],
-					modifier: [],
-					allies: [],
+					quest: [],
+					neutral: [],
 					antagonists: [],
 					locations: []
 				}
 			}
 		},
     methods: {
-			makeNew(field) {
+			makeStarter() {
 				let nums = Math.round(Math.random() * 3) + 1
-				this.values[field] = []
+				this.values['starter'] = []
 				for (var index = 0; index < nums; index++) {
-					let newValue = this.quests.getRandom(field)
-					if ( ! this.values[field].includes(newValue)) {
-						this.values[field].push( newValue )
+					this.values['starter'].push( this.quests.getStarter() )
+				}
+			},
+			makeQuest() {
+				this.values['quest'] = this.quests.getQuest()
+			},
+			makeNeutral() {
+				let nums = Math.round(Math.random() * 2) + 1
+				this.values['neutral'] = []
+				for (var index = 0; index < nums; index++) {
+					this.values['neutral'].push( this.quests.getNeutral() )
+				}
+			},
+			makeAntagonist() {
+				let nums = Math.round(Math.random() * 4) + 1
+				this.values['antagonists'] = []
+				for (var index = 0; index < nums; index++) {
+					this.values['antagonists'].push( this.quests.getAntagonist() )
+				}
+			},
+			makeLocation() {
+				let nums = Math.round(Math.random() * 2) + 1
+				this.values['locations'] = []
+				for (var index = 0; index < nums; index++) {
+					let newValue = this.quests.getLocation()
+					if ( ! this.values.locations.includes(newValue)) {
+						this.values.locations.push( newValue )
 					}
 				}
 			},
 			regenerate() {
-				this.makeNew('starter')
-				this.makeNew('type')
-				this.makeNew('modifier')
-				this.makeNew('allies')
-				this.makeNew('antagonists')
-				this.makeNew('locations')
+				this.makeStarter()
+				this.makeQuest()
+				this.makeNeutral()
+				this.makeAntagonist()
+				this.makeLocation()
       }
 		},
 		mounted() {
--- src/data/templates/quests.js
+++ src/data/templates/quests.js
@@ -1,41 +1,128 @@
+import Locations from "./quests/location"
+import Npc from "./quests/npc"
+import Trait from "./quests/trait"
 export default {
-  starter: [
-    'ambassador',
-    'artificial intelligence',
-    'celebrity',
-    'clergyman',
-    'companion',
-    'corporation',
-    'criminal',
-    'current news story',
-    'diplomat',
-    'distress beacon',
-    'foriegner',
-    'free-trader',
-    'friend',
-    'government official',
-    'information broker',
-    'intelligence report',
-    'law enforcement officer',
-    'local comms chatter',
-    'local community',
-    'local rumour',
-    'mercenaries',
-    'middleman',
-    'military officer',
-    'naval officer',
-    'overheard conversation',
-    'politician',
-    'powerbroker',
-    'relation',
-    'rival',
-    'scholar',
-    'scientist',
-    'sensor reading',
-    'soldier',
-    'trade union',
-  ],
-  type: [
+  getArtificial() {
+    // The template
+    let starter = {
+      name: "",
+      traits: []
+    }
+    // Select an option at random
+    starter.name = Npc.artificial[Math.round(Math.random() * (Npc.artificial.length -1))]
+    // Determine the traits
+    let number_of_traits = Math.round(Math.random() * 2) + 1
+    for (var i = 0; i < number_of_traits; i++) {
+      let newValue = Trait.artificial[Math.round(Math.random() * (Trait.artificial.length -1))]
+      if ( ! starter.traits.includes(newValue)) {
+        starter.traits.push( newValue )
+      }
+    }
+    // Return the object
+    return starter
+  },
+  getAlly() {
+    // The template
+    let starter = {
+      name: "",
+      traits: []
+    }
+    // Combined arrays
+    let combined = [].concat(Npc.job, Npc.civilian, Npc.rogue)
+    // Select an option at random
+    starter.name = combined[Math.round(Math.random() * (combined.length -1))]
+    // Determine the traits
+    let number_of_traits = Math.round(Math.random() * 3) + 1
+    for (var i = 0; i < number_of_traits; i++) {
+      let newValue = Trait.biological[Math.round(Math.random() * (Trait.biological.length -1))]
+      if ( ! starter.traits.includes(newValue)) {
+        starter.traits.push( newValue )
+      }
+    }
+    // Return the object
+    return starter
+  },
+  getEnemy() {
+    // The template
+    let enemy = {
+      name: "",
+      traits: []
+    }
+    // Combined arrays
+    let combined = [].concat(Npc.illegal, Npc.rogue)
+    // Select an option at random
+    enemy.name = combined[Math.round(Math.random() * (combined.length -1))]
+    // Determine the traits
+    let number_of_traits = Math.round(Math.random() * 3) + 1
+    for (var i = 0; i < number_of_traits; i++) {
+      let newValue = Trait.biological[Math.round(Math.random() * (Trait.biological.length -1))]
+      if ( ! enemy.traits.includes(newValue)) {
+        enemy.traits.push( newValue )
+      }
+    }
+    // Return the object
+    return enemy
+  },
+  getGroup() {
+    // The template
+    let starter = {
+      name: "",
+      traits: []
+    }
+    // Select an option at random
+    starter.name = Npc.group[Math.round(Math.random() * (Npc.group.length -1))]
+    // Determine the traits
+    let number_of_traits = Math.round(Math.random() * 2) + 1
+    for (var i = 0; i < number_of_traits; i++) {
+      let newValue = Trait.group[Math.round(Math.random() * (Trait.group.length -1))]
+      if ( ! starter.traits.includes(newValue)) {
+        starter.traits.push( newValue )
+      }
+    }
+    // Return the object
+    return starter
+  },
+  getOther() {
+    // The template
+    let starter = {
+      name: "",
+      traits: []
+    }
+    // Select an option at random
+    starter.name = Npc.source[Math.round(Math.random() * (Npc.source.length -1))]
+    // Return the object
+    return starter
+  },
+  /**
+   * Quest Starter format
+   * {
+   *    name: "String",
+   *    traits: [
+   *      "List of trait strings"
+   *    ]
+   * }
+   */
+  getStarter() {
+    let name = Math.round(Math.random() * 3) + 1
+    if (name == 1) {
+      return this.getArtificial()
+    } else if ( name == 2) {
+      return this.getGroup()
+    } else if ( name == 3) {
+      return this.getOther()
+    }
+    return this.getAlly()
+  },
+  /**
+   * Quest format
+   * {
+   *    name: "String",
+   *    traits: [
+   *      "List of trait strings"
+   *    ]
+   * }
+   */
+  quest: [
     'collection',
     'courier',
     'discovery',
@@ -55,122 +142,70 @@ export default {
     'survival',
     'track',
   ],
-  modifier: [
-    'alien',
-    'ambush',
-    'barely legal',
-    'betrayal',
-    'blackmail',
-    'deadly environment',
-    'deception',
-    'disguise',
-    'dishonesty',
-    'extremely profitable',
-    'feud',
-    'great distance',
-    'highly profitable',
-    'illegal',
-    'lack of supplies / resources',
-    'limited time',
-    'lost',
-    'medical / biological',
-    'misleading information',
-    'more than one faction',
-    'mysterious',
-    'natural disaster',
-    'personal',
-    'religious / spiritual',
-    'revenge',
-    'righteous',
-    'scientific / technical',
-    'secret',
-    'standard',
-    'stolen',
-    'unethical',
-  ],
-  allies: [
-    'artificial intelligence',
-    'artist',
-    'astronomer',
-    'civilian',
-    'entertainer',
-    'escaped prisoner',
-    'escaped slave',
-    'explorers',
-    'free-trader',
-    'honourable mercenaries',
-    'law enforcement',
-    'naive youth',
-    'pilot',
-    'redeemed criminal',
-    'refugees',
-    'rogue adventurer',
-    'scavengers',
-    'scholar',
-    'scientist',
-    'streetwise urchins',
-    'technician',
-    'victim who wants revenge',
-  ],
-  antagonists: [
-    'assassins',
-    'bandits',
-    'bounty hunter',
-    'corporation',
-    'corrupt official',
-    'criminals',
-    'cultists',
-    'greedy free-trader',
-    'law enforcement',
-    'madman',
-    'mercenaries',
-    'military officer',
-    'naval officer',
-    'obsessed scientist',
-    'pirates',
-    'rebels',
-    'rich entreprenuer',
-    'rival',
-    'rogue artificial intelligence',
-    'slaver',
-    'theives',
-    'trade guild',
-    'warlord',
-  ],
-  locations: [
-    'abandoned space craft/station',
-    'academy',
-    'apartment block',
-    'bandit territory',
-    'brothel',
-    'casino',
-    'catacombs',
-    'caves',
-    'corporate office',
-    'damaged space craft/station',
-    'deserted outpost',
-    'dive bar',
-    'gambling den',
-    'garden or park',
-    'industrial factory',
-    'luxury residence',
-    'marketplace',
-    'medical facility',
-    'military outpost',
-    'military space craft/station',
-    'mines',
-    'night club',
-    'prison',
-    'religious site',
-    'remote wilderness',
-    'sanctuary',
-    'scientific facility',
-    'space station',
-    'strip club',
-    'warehouse',
-  ],
-  getRandom(field) {
-    let valueArray = this[field]
-    return valueArray[Math.round(Math.random() * (valueArray.length - 1))]
+  getQuest() {
+    // The template
+    let quest = {
+      name: "",
+      traits: []
+    }
+    // Select an option at random
+    quest.name = this.quest[Math.round(Math.random() * (this.quest.length -1))]
+    // Is there a side mission component?
+    if (Math.round(Math.random())) {
+      let side = this.quest[Math.round(Math.random() * (this.quest.length -1))]
+      if (side !== quest.name) {
+        quest.name += " / " + side
+      }
+    }
+    // Determine the traits
+    let number_of_traits = Math.round(Math.random()) + 1
+    for (var i = 0; i < number_of_traits; i++) {
+      let newValue = Trait.quest[Math.round(Math.random() * (Trait.quest.length -1))]
+      if ( ! quest.traits.includes(newValue)) {
+        quest.traits.push( newValue )
+      }
+    }
+    // Return the object
+    return quest
+  },
+  /**
+   * Quest neutral format
+   * {
+   *    name: "String",
+   *    traits: [
+   *      "List of trait strings"
+   *    ]
+   * }
+   */
+  getNeutral() {
+    let name = Math.round(Math.random() * 2) + 1
+    if (name == 1) {
+      return this.getArtificial()
+    } else if ( name == 2) {
+      return this.getGroup()
+    }
+    return this.getAlly()
+  },
+  /**
+   * Quest antagonist format
+   * {
+   *    name: "String",
+   *    traits: [
+   *      "List of trait strings"
+   *    ]
+   * }
+   */
+  getAntagonist() {
+    let name = Math.round(Math.random() * 2) + 1
+    if (name == 1) {
+      return this.getGroup()
+    }
+    return this.getEnemy()
+  },
+  // Locations only have it's type
+  getLocation() {
+    let combined = [].concat(Locations.rural, Locations.space, Locations.urban)
+    // Select an option at random
+    return combined[Math.round(Math.random() * (combined.length -1))]
   }
 }
