3506e5f81f9429a341352bd25530602a7fc43ddd
Nassar Zeitoune <nassar.z@slair.com.au>
2018-06-20 09:40:17 +1000

    Added descriptions to the nation summary sheet

--- src/assets/vue/detail-row/nation-summary.vue
+++ src/assets/vue/detail-row/nation-summary.vue
@@ -4,9 +4,11 @@
       <div><strong>Name</strong></div>
       <div class="ml-4 mt-1 mb-1 bottom-border">{{ nation.name }}</div>
       <div><strong>Status</strong></div>
-      <div class="ml-4 mt-1 mb-1 bottom-border">{{ nation.governance.status | capitalize }}</div>
+      <div class="ml-4 mt-1 mb-1 bottom-border">{{ nation.governance.status | capitalize }} <help-generic :popover="'status-details'">{{ regions.governance.government.status[nation.governance.status] }}</help-generic></div>
       <div><strong>Government</strong></div>
-      <div class="ml-4 mt-1 mb-1 bottom-border">{{ nation.governance.source | capitalize}} {{ nation.governance.structure | capitalize}}</div>
+      <div class="ml-4 mt-1 mb-1 bottom-border">{{ nation.governance.source | capitalize}} <help-generic :popover="'governance-source-details'">{{ regions.governance.government.source[nation.governance.source ]}}</help-generic> {{ nation.governance.structure | capitalize}} <help-generic :popover="'governance-structure-details'">{{ regions.governance.government.structure[nation.governance.structure ]}}</help-generic></div>
+      <div><strong>Description</strong></div>
+      <div class="ml-4 mt-1 mb-1 bottom-border">{{ nation.description }}</div>
     </v-card-text>
   </v-card>
 </template>
@@ -17,6 +19,11 @@
         type: Object,
         required: true
       }
-    }
+    },
+    data() {
+			return {
+				regions: this.$bsFactory.getTemplate('regions'),
+			}
+    },
   }
 </script>
